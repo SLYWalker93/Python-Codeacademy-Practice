aa3d1cbb7c95391d35179e0386609a0bf2ae7908
SLYWalker93 <notorioustw@gmail.com>
2015-10-13 21:49:07 -0700

    FUCK CODEACADEMY
    I DID IT RIGHT

---
+++ IDKK.py
@@ -0,0 +1,21 @@
+import os
+
+
+
+print "HELLO! CODEACADEMY PISSES ME OFF!!"
+print " "
+
+start_list = [5, 3, 1, 2, 4]
+
+square_list=[]
+
+square_list.append(start_list[0]**2)
+square_list.append(start_list[1]**2)
+square_list.append(start_list[2]**2)
+square_list.append(start_list[3]**2)
+square_list.append(start_list[4]**2)
+
+square_list.sort()
+for start_list in square_list:
+
+    print square_list
